43c389664ee6fab6688e684a26a0a9a41a32c6c9
Craig R. Hughes <craig.git@rungie.com>
2024-04-19 21:54:56 -0700

    Try out some semantic chunking - seems to work decently!

--- parseNovel.mjs
+++ parseNovel.mjs
@@ -1,25 +1,39 @@
 import { OllamaEmbeddings } from '@langchain/community/embeddings/ollama';
+import { CacheBackedEmbeddings } from "langchain/embeddings/cache_backed";
+import { InMemoryStore } from "langchain/storage/in_memory";
 import { FaissStore } from '@langchain/community/vectorstores/faiss';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { Document } from "langchain/document";
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { inspect } from 'node:util';
 import cliProgress from 'cli-progress';
+import { SemanticTextSplitter } from './lib/SemanticChunker.mjs';
 
 const book = 'Christmas Town draft 2';
-
-const loader = new PDFLoader(`novels/${book}.pdf`, { splitPages: true, });
+const loader = new PDFLoader(`novels/${book}.pdf`, { splitPages: false, });
 // const loader = new TextLoader(`novels/${book}.txt`);
 const docs = await loader.load();
+// const docs = [new Document({ pageContent: 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of light, it was the season of darkness, it was the spring of hope, it was the winter of despair.' })];
 
-const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1024,
-    chunkOverlap: 64,
+const fastEmbeddings = new OllamaEmbeddings({ model: 'nomic-embed-text', numCtx: 2048 });
+const slowEmbeddings = new OllamaEmbeddings({ model: 'mistral:instruct', numCtx: 32768 });
+
+const embeddings = CacheBackedEmbeddings.fromBytesStore(
+    fastEmbeddings,
+    new InMemoryStore(),
+    {
+        namespace: fastEmbeddings.modelName,
+    }
+)
+
+const splitter = new SemanticTextSplitter({
+    showProgress: true,
+    embeddings,
 });
 
 const splits = await splitter.splitDocuments(docs);
-
-const embeddings = new OllamaEmbeddings({ model: 'nomic-embed-text', numCtx: 2048 });
+// console.log(splits);
 
 let vectorStore;
 
@@ -29,7 +43,7 @@ for(let split of splits) {
     if(vectorStore) {
         await vectorStore.addDocuments([split]);
     } else {
-        vectorStore = await FaissStore.fromDocuments([split], embeddings);
+        vectorStore = await FaissStore.fromDocuments([split], slowEmbeddings);
     }
     bar.increment();
 }
