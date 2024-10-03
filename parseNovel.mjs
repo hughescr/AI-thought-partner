acaae224725dc4ad7a2ab7df2cfdb15d5d3bcdee
Craig R. Hughes <craig.git@rungie.com>
2024-10-03 10:41:01 -0700

    Update parseNovel to use better embeddings; cleanup imports

--- parseNovel.mjs
+++ parseNovel.mjs
@@ -4,12 +4,10 @@ import { InMemoryStore } from "langchain/storage/in_memory";
 import { FaissStore } from '@langchain/community/vectorstores/faiss';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-import { Document } from "langchain/document";
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-import { inspect } from 'node:util';
 import cliProgress from 'cli-progress';
 import { SemanticTextSplitter } from './lib/SemanticChunker.mjs';
-import _, { initial } from 'lodash';
+import _ from 'lodash';
+import { logger } from '@hughescr/logger';
 
 const book = 'Christmas Town beta';
 // const loader = new PDFLoader(`novels/${book}.pdf`, { splitPages: true, });
@@ -18,10 +16,12 @@ const docs = await loader.load();
 
 // const docs = [new Document({ pageContent: 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of light, it was the season of darkness, it was the spring of hope, it was the winter of despair.' })];
 
-// const coreEmbeddings = new OllamaEmbeddings({ model: 'nomic-embed-text', numCtx: 2048 });
-const coreEmbeddings = new OllamaEmbeddings({ model: 'mxbai-embed-large', numCtx: 512 });
-// const coreEmbeddings = new OllamaEmbeddings({ model: 'mistral:7b-instruct-v0.2-q8_0', numCtx: 32768 });
-// const coreEmbeddings = new OllamaEmbeddings({ model: 'llama3.1:8b-instruct-q8_0', numCtx: 2048 });
+// const coreEmbeddings = new OllamaEmbeddings({ model: 'nomic-embed-text', requestOptions: { numCtx: 2048 } });
+// const coreEmbeddings = new OllamaEmbeddings({ model: 'mxbai-embed-large', requestOptions: { numCtx: 512 } });
+// const coreEmbeddings = new OllamaEmbeddings({ model: 'bge-large', requestOptions: { numCtx: 512 } });
+const coreEmbeddings = new OllamaEmbeddings({ model: 'bge-m3', requestOptions: { numCtx: 8192 } });
+// const coreEmbeddings = new OllamaEmbeddings({ model: 'mistral:7b-instruct-v0.2-q8_0', requestOptions: { numCtx: 32768 } });
+// const coreEmbeddings = new OllamaEmbeddings({ model: 'llama3.1:8b-instruct-q8_0', requestOptions: { numCtx: 2048 } });
 
 const embeddings = CacheBackedEmbeddings.fromBytesStore(
     coreEmbeddings,
@@ -39,12 +39,12 @@ const splitter = new SemanticTextSplitter({
 });
 
 const splits = await splitter.splitDocuments(docs);
-// console.log(inspect(splits, { depth: null }));
+// logger.info(splits);
 
 let vectorStore;
 
 const bar = new cliProgress.SingleBar({ barsize: 80, format: '{bar} {value}/{total} splits | {percentage}% | Time: {duration_formatted} | ETA: {eta_formatted}'}, cliProgress.Presets.shades_classic);
-bar.start(splits.length, 0);
+bar.start(_.flatten(splits).length, 0);
 for(let split of splits) {
     if(vectorStore) {
         await vectorStore.addDocuments([split]);
