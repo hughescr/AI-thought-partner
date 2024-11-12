46fa3f403123b715af334bec13828844281cf289
Craig R. Hughes <craig.git@rungie.com>
2024-11-12 15:10:03 -0800

    Clarify licensing on all models

--- lib/LLMs.ts
+++ lib/LLMs.ts
@@ -17,36 +17,40 @@ const makeCachedEmbeddings = (model: OllamaEmbeddings) => CacheBackedEmbeddings.
     }
 );
 
-// Nomic uses Apache License 2.0
+// Apache License 2.0
 export const nomicEmbeddings = new OllamaEmbeddings({ model: 'nomic-embed-text', requestOptions: { numCtx: 2048 } });
 export const cachedNomicEmbeddings = makeCachedEmbeddings(nomicEmbeddings);
-
-// Jina uses Apache License 2.0
 export const jinaV2SmallENEmbeddings = new OllamaEmbeddings({ model: 'jina/jina-embeddings-v2-small-en', requestOptions: { numCtx: 8192 } });
 export const cachedJinaV2SmallENEmbeddings = makeCachedEmbeddings(jinaV2SmallENEmbeddings);
-
-// Jina uses Apache License 2.0
 export const jinaV2BaseENEmbeddings = new OllamaEmbeddings({ model: 'jina/jina-embeddings-v2-base-en', requestOptions: { numCtx: 8192 } });
 export const cachedJinaV2BaseENEmbeddings = makeCachedEmbeddings(jinaV2BaseENEmbeddings);
 
-// BGE M3 uses MIT License
+// MIT License
 export const bgeM3Embeddings = new OllamaEmbeddings({ model: 'bge-m3', requestOptions: { numCtx: 8192 }});
 export const cachedBgeM3Embeddings = makeCachedEmbeddings(bgeM3Embeddings);
 
 // LLMs
+
+// Apache License 2.0
 export const qwen25_1_5bLLM = new ChatOllama({ model: 'qwen2.5:1.5b-instruct-fp16', ...commonOptions32k });
-export const llama32_3bLLM = new ChatOllama({ model: 'llama3.2:3b-instruct-fp16', ...commonOptions32k });
-export const phi35_4bLLM = new ChatOllama({ model: 'phi3.5:3.8b-mini-instruct-fp16', ...commonOptions32k });
-export const llama31_8bLLM = new ChatOllama({ model: 'llama3.1:8b-instruct-q8_0', ...commonOptions64k });
-export const nemo_12bLLM = new ChatOllama({ model: 'mistral-nemo:12b-instruct-2407-q8_0', ...commonOptions32k });
 export const qwen25_14bLLM = new ChatOllama({ model: 'qwen2.5:14b-instruct-q8_0', ...commonOptions32k });
-export const phi3_14bLLM = new ChatOllama({ model: 'phi3:14b-medium-128k-instruct-q8_0', ...commonOptions32k });
 export const qwen25_32bLLM = new ChatOllama({ model: 'qwen2.5:32b-instruct-q8_0', ...commonOptions32k });
+export const nemo_12bLLM = new ChatOllama({ model: 'mistral-nemo:12b-instruct-2407-q8_0', ...commonOptions32k });
+
+// MIT License
+export const phi35_4bLLM = new ChatOllama({ model: 'phi3.5:3.8b-mini-instruct-fp16', ...commonOptions32k });
+export const phi3_14bLLM = new ChatOllama({ model: 'phi3:14b-medium-128k-instruct-q8_0', ...commonOptions32k });
+
+// Llama Community License
+export const llama32_3bLLM = new ChatOllama({ model: 'llama3.2:3b-instruct-fp16', ...commonOptions32k });
+export const llama31_8bLLM = new ChatOllama({ model: 'llama3.1:8b-instruct-q8_0', ...commonOptions64k });
 export const llama31_70bLLM = new ChatOllama({ model: 'llama3.1:70b-instruct-q8_0', ...commonOptions32k });
+
+// CC-Attribution-Non-Commercial
 export const bespokeMinicheckLLM = new ChatOllama({ model: 'bespoke-minicheck:7b-q8_0', ...commonOptions32k });
 
 // Rerankers
-// Jina uses Apache License 2.0
+
+// Apache License 2.0
 export const jinaV1TinyENReranker = new OllamaRerank({ model: 'jina-reranker-v1-tiny-en:bf16', topN: 10 });
-// BGE M3 uses Apache License 2.0
 export const bgeV2M3Reranker = new OllamaRerank({ model: 'bge-reranker-v2-m3:bf16', topN: 5 });
