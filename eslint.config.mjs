3abac437746f5f51e2bbfab04054146cac412de2
Craig R. Hughes <craig.git@rungie.com>
2024-11-14 03:36:45 -0800

    Fix linting for typescript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 // eslint-disable-next-line n/no-unpublished-import -- This import is not published cos it's dev only
 import defaultConfig from '@hughescr/eslint-config-default';
+// eslint-disable-next-line n/no-unpublished-import -- This import is not published cos it's dev only
+import tseslint from 'typescript-eslint';
 
 export default
 [
@@ -8,4 +10,16 @@ export default
         ignores: ['coverage', 'node_modules', 'venv'],
     },
     defaultConfig.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.stylistic,
+    {
+        rules: {
+            '@stylistic/space-before-blocks': ['warn', { functions: 'always', keywords: 'always', classes: 'always' }],
+            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        },
+    },
+    {
+        files: ['**/*.js', '**/*.mjs'],
+        ...tseslint.configs.disableTypeChecked,
+    },
 ];
