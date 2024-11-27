e3fd6106c1b080a44b41998cc35e46200ff0297e
Craig R. Hughes <craig.git@rungie.com>
2024-11-26 18:17:03 -0800

    feat: Integrate semantic text splitter and FAISS store for novel parsing

--- parseNovelGraph.ts
+++ parseNovelGraph.ts
@@ -4,9 +4,22 @@ import {
     nemo_12bLLM as fastDumbLLM,
 } from './lib/LLMs.ts';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
-import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
+import { SemanticTextSplitter } from './lib/SemanticTextSplitter.ts';
+import { FaissStoreWithMMR } from './lib/FAISSStoreWithMMR.ts';
+import { END, START, StateGraph, Annotation } from '@langchain/langgraph';
 import neo4j, { Driver, Session } from 'neo4j-driver';
 import z from 'zod';
+import { logger } from '@hughescr/logger';
+import _ from 'lodash';
+import chalk from 'chalk';
+
+if (process.versions.bun === undefined) {
+    logger.info(chalk.greenBright('Running under Node, setting global dispatcher'));
+    const { setGlobalDispatcher, Agent } = await import('undici');
+    setGlobalDispatcher(new Agent({ headersTimeout: 0, bodyTimeout: 0 })); // ensure we wait for long ollama runs
+} else {
+    logger.warn(chalk.yellowBright('Running under Bun, not setting global dispatcher so LLMs might timeout'));
+}
 
 const neo4jURL = process.env.NEO4J_URI || '';
 const neo4jUsername = process.env.NEO4J_USER || '';
@@ -16,12 +29,21 @@ const driver: Driver = neo4j.driver(
     neo4jURL,
     neo4j.auth.basic(neo4jUsername, neo4jPassword)
 );
-const textSplitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 8 * 1024,
-    keepSeparator: true,
-    separators: ['##'],
+
+const splitter = new SemanticTextSplitter({
+    showProgress: false,
+    initialChunkSize: 32, // Tokens!
+    chunkSize: 2048, // Tokens!
+    embeddings: embeddings, // Use fast embeddings for decent semantic splits
+    embeddingBatchSize: 128,
 });
 
-const book = 'Christmas town beta';
+const book = 'Christmas Town beta';
+const storeDirectory = `novels/${book}`;
 const loader: TextLoader = new TextLoader(`novels/${book}.md`);
 const novelText = await loader.load();
+
+const vectorStore = await FaissStoreWithMMR.load(
+    storeDirectory,
+    embeddings
+);
