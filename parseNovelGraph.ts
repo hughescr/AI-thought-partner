4039cd39ea1de5f66939e4dfda92b5d4c0d292f5
Craig R. Hughes <craig.git@rungie.com>
2024-11-27 03:03:58 -0800

    refactor: Remove unnecessary closing brackets in entity refinement functions

--- parseNovelGraph.ts
+++ parseNovelGraph.ts
@@ -472,11 +472,10 @@ async function refineEntitiesWithContext(state: { novelChunk: string, entities: 
 
         progressBar.update(index + 1);
         return refinementResult;
-    });
+    }));
 
     progressBar.stop();
     logger.info(chalk.green('Entities refined with context.'));
-    }));
 
     return { entities: refinedEntities };
 }
@@ -513,7 +512,6 @@ async function furtherRefineEntities(state: { entities: Entity[] }): Promise<{ e
 
     progressBar.stop();
     logger.info(chalk.green('Entities further refined using Neo4j.'));
-    }));
 
     return { entities: updatedEntities };
 }
