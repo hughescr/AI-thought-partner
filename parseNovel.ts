7fc537ab519b5b4e5620e2128764292a2c70a7b9
Craig R. Hughes <craig.git@rungie.com>
2025-02-11 09:38:22 -0800

    Construct NovelDocument from the read markdown document; use local LLM for summarization

--- parseNovel.ts
+++ parseNovel.ts
@@ -1,11 +1,13 @@
 import {
-    novaLiteLLM as summarizerLLM,
+    deepseekR1_Qwen32bLLM as summarizerLLM,
     cachedSnowflakeArctic2Embeddings
 } from './lib/LLMs';
 
 import { NovelDocument, NovelDocumentStore } from './lib/NovelDocumentStore';
 import { ChapterSummaryGenerator } from './lib/ChapterSummaryGenerator';
 
+// import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+// import { DocxLoader } from '@langchain/community/document_loaders/fs/docx';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
 
 import _ from 'lodash';
@@ -13,25 +15,27 @@ import { MultiBar, Presets as cliProgressPresets } from 'cli-progress';
 
 const book = 'Christmas Town query version';
 // const loader = new PDFLoader(`novels/${book}.pdf`, { splitPages: true });
+// const loader = new DocxLoader(`novels/${book}.docx`);
 const loader = new TextLoader(`novels/${book}.md`);
 const docs = await loader.load();
-const novel = docs[0] as NovelDocument;
-novel.metadata.title = book;
-novel.metadata.author = 'Erica S. Hughes';
-novel.metadata.genre = 'Young adult';
+const novel = NovelDocument.fromDocument(docs[0], book, 'Erica S. Hughes', 'Young Adult', loader.filePathOrBlob.toString());
 
 const summaryGenerator = new ChapterSummaryGenerator({
     llm: summarizerLLM,
     targetSummarySize: 256,
 });
+
 const bars = new MultiBar({
     clearOnComplete: true,
     hideCursor: false,
     format: '{bar} {percentage}% | {duration_formatted} | ETA: {eta_formatted} | {value}/{total} | {msg}',
 }, cliProgressPresets.shades_classic);
-const novelStore = new NovelDocumentStore(cachedSnowflakeArctic2Embeddings, { filePath: 'novels.db', summaryGenerator: summaryGenerator, debugBar: bars });
-await novelStore.addNovel(novel);
-
-await novelStore.close();
-
+const novelStore = new NovelDocumentStore(cachedSnowflakeArctic2Embeddings, { filePath: 'novels_db', summaryGenerator: summaryGenerator, debugBar: bars });
+if(!(await novelStore.getNovel(novel.metadata.title, novel.metadata.author))) {
+    await novelStore.addNovel(novel);
+}
 bars.stop();
+
+console.log("Novel:", novel.metadata);
+const novelSummary = await novelStore.getNovelSummary(novel.metadata.novelID);
+console.log("Novel Summary:", novelSummary?.pageContent);
